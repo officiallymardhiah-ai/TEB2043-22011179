036b5b119749156c53799a457dfcd0f230306b34
officiallymardhiah-ai <officiallymardhiah@gmail.com>
2026-03-30 17:35:58 +0800

    Add lab1b.R for submission

---
+++ lab1b.R
@@ -0,0 +1,14 @@
+# Question 1
+numbers <- 1:20
+print(numbers)
+print(numbers^2)
+
+# Question 2
+num1 <- 0.956786
+num2 <- 7.8345901
+print(round(num1, 2))
+print(round(num2, 3))
+
+# Question 3
+radius <- as.numeric(readline("Enter radius: "))
+print(pi * radius^2) 
